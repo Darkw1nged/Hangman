56ecbfd6922155b4dba96ea21178539d1bcd8f38
Darkwinged <56140296+Darkw1nged@users.noreply.github.com>
2024-10-13 17:32:10 +0100

    added Loading

--- scripts/wordGeneration.js
+++ scripts/wordGeneration.js
@@ -2,7 +2,7 @@ let word = '';
 
 // Function to get words from the JSON file using Fetch API
 function getWordJSON() {
-    return fetch('/Hangman/scripts/data/words.json')
+    return fetch('https://darkw1nged.github.io/Hangman/scripts/data/words.json')
         .then(response => {
             if (!response.ok) {
                 throw new Error(`HTTP error! status: ${response.status}`);
@@ -18,21 +18,33 @@ function getWordJSON() {
         });
 }
 
-// Function to generate a random word
+function showLoading() {
+    document.querySelector('.loading').style.display = 'block';
+}
+
+function hideLoading() {
+    document.querySelector('.loading').style.display = 'none';
+}
+
 function generateWord() {
+    showLoading();
+
     getWordJSON().then(words => {
-        // console.log('Fetched words:', words);
+        hideLoading();
 
         if (words.length > 0) {
             var randomIndex = Math.floor(Math.random() * words.length);
             word = words[randomIndex];
-            // console.log('Selected word:', word);
             generateWordDivs(word);
         } else {
             console.error('No words available');
         }
+    }).catch(error => {
+        hideLoading();
+        console.error('Error fetching words:', error);
     });
 }
+
 
 // Generate divs respective to the length of the word with class .letter
 function generateWordDivs(word) {
