4256cee332800c17db4096fc2072af5ac1bb7a61
Darkwinged <56140296+Darkw1nged@users.noreply.github.com>
2024-10-24 17:17:05 +0100

    Setup found words logic

---
+++ scripts/storage.js
@@ -0,0 +1,23 @@
+// Per category i want to store all words found and the amount of words comapred to the total amount of words in that category.
+function storeWord(word, category) {
+    if (!localStorage.getItem('words')) {
+        localStorage.setItem('words', JSON.stringify({}));
+    }
+
+    const words = JSON.parse(localStorage.getItem('words'));
+    if (!words[category]) {
+        words[category] = { found: [], total: 0 };
+    }
+
+    // If word is already found, do nothing
+    if (words[category].found.includes(word)) {
+        console.log('Word already found');
+        return;
+    }
+
+    words[category].found.push(word);
+    words[category].total++;
+
+    localStorage.setItem('words', JSON.stringify(words));
+    console.log('Word stored:', word);
+}
